a43f3a14d3a8ce8ea6e9f841d3b044cca9db3d7a
HongQiLi <hongqi.li@colorado.edu>
2025-08-05 03:01:04 -0600

    Create __init__.py

---
+++ routers/__init__.py
@@ -0,0 +1,15 @@
+# routers/__init__.py
+# 初始化 routers 包 / Initialize routers package
+
+# 这个文件的存在意味着 routers 是一个 Python 包
+# This file tells Python that `routers` is a package
+
+# 你可以在这里导入所有模块，也可以不写任何代码
+# You may import all submodules here (optional)
+
+# from .user import router as user_router
+# from .schedule import router as schedule_router
+# from .recommender import router as recommender_router
+# from .feedback import router as feedback_router
+# from .parent import router as parent_router
+# from .calendar_sync import router as calendar_sync_router
